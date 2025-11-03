cb640ffc1709fb2a3f3944f8533e9b11c113acc2
gurkirat309 <jaggigs309@gmail.com>
2025-11-03 22:55:08 +0530

    config

--- networksecurity/entity/config_entity.py
+++ networksecurity/entity/config_entity.py
@@ -33,4 +33,4 @@ class DataIngestionConfig:
             )
         self.train_test_split_ratio: float = training_pipeline.DATA_INGESTION_TRAIN_TEST_SPLIT_RATION
         self.collection_name: str = training_pipeline.DATA_INGESTION_COLLECTION_NAME
-        self.database_name: str = training_pipeline.DATA_INGESTION_DATABASE_NAME
+        self.database_name: str = training_pipeline.DATA_INGESTION_DATABASE_NAME 
